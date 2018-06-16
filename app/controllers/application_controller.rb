61e21a2cc82c45ccac437f58bcf7a503b086fac5
Paul <Paul@storberg.net>
2018-06-16 16:55:34 -0400

    minor edits and signup routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery with: :exception
+
+  protected
+
+  def after_sign_in_path_for(resource)
+    request.env['omniauth.origin'] || dashboard_index_path(resource) || root_path
+  end
 end
