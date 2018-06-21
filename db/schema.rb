fdd2c31265b76e6bafa8f7e4067be88fa7cac17a
Paul <Paul@storberg.net>
2018-06-21 16:12:14 -0400

    added time stamps to projects

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_20_235541) do
+ActiveRecord::Schema.define(version: 2018_06_21_200309) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,8 @@ ActiveRecord::Schema.define(version: 2018_06_20_235541) do
 
   create_table "projects", force: :cascade do |t|
     t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "roles", id: :serial, force: :cascade do |t|
