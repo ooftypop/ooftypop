6cf433826b46b23ec446eea6a8abdb09f28e1bba
Paul <Paul@storberg.net>
2018-06-20 20:05:14 -0400

    created projects table, controller and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_14_161014) do
+ActiveRecord::Schema.define(version: 2018_06_20_235541) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,10 @@ ActiveRecord::Schema.define(version: 2018_06_14_161014) do
     t.text "description"
     t.string "title"
     t.string "url"
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.string "title"
   end
 
   create_table "roles", id: :serial, force: :cascade do |t|
