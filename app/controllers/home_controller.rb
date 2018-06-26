68ed4b4e536917940ada121df034652103ec43a2
Paul Storberg <Paul@storberg.net>
2018-06-26 10:30:39 -0400

    added will paginate gem

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
   def index
     @dispatched_email = DispatchedEmail.new
     @portfolio_item   = PortfolioItem.all
+    @portfolio_items = PortfolioItem.paginate(:page => params[:page], :per_page => 8)
   end
 end
