0ba0057575ff7a4e3ddceccda24918ad06b41a05
Paul Storberg <Paul@storberg.net>
2018-06-28 20:12:05 -0400

    added scroll to overflow on mobile widths for portfolio items

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,5 @@ class HomeController < ApplicationController
   def index
     @dispatched_email = DispatchedEmail.new
     @portfolio_item   = PortfolioItem.all
-    @portfolio_items = PortfolioItem.paginate(:page => params[:page], :per_page => 8)
   end
 end
