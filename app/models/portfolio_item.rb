e8509168ea5da07ecfebb7bac072691239b2da78
Ryan Storberg <ryan@storberg.net>
2018-07-05 11:16:51 +0700

    Update portfolio_item.rb

--- app/models/portfolio_item.rb
+++ app/models/portfolio_item.rb
@@ -1,8 +1,3 @@
 class PortfolioItem < ApplicationRecord
-
-  validates :description, presence: true
-  validates :published,   presence: true
-  validates :title,       presence: true
-  validates :url,         presence: true
-
+  validates :description, :published, :title, presence: true
 end
