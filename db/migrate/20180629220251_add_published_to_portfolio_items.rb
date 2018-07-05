954b544fb7ebc36899f4f5be9355402942dd23cf
Ryan Storberg <ryan@storberg.net>
2018-07-05 11:17:28 +0700

    Update 20180629220251_add_published_to_portfolio_items.rb

--- db/migrate/20180629220251_add_published_to_portfolio_items.rb
+++ db/migrate/20180629220251_add_published_to_portfolio_items.rb
@@ -1,5 +1,5 @@
 class AddPublishedToPortfolioItems < ActiveRecord::Migration[5.2]
   def change
-    add_column :portfolio_items, :published, :boolean
+    add_column :portfolio_items, :published, :boolean, default: false
   end
 end
