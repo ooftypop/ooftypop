26eed812ffebc7087691fc3df093e607843d1272
Paul Storberg <Paul@storberg.net>
2018-07-13 12:31:10 -0400

    Added devise_invitable to app

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -29,4 +29,24 @@ ActiveAdmin.register User do
     f.actions
   end
 
+  action_item do
+      link_to 'Invite New User', new_invitation_admin_users_path
+  end
+
+  collection_action :new_invitation do
+      @user = User.new
+      end
+
+  collection_action :send_invitation, :method => :post do
+      @user = User.invite!(permitted_params[:user], current_user)
+      if @user.errors.empty?
+          flash[:success] = "User has been successfully invited."
+          redirect_to admin_users_path
+      else
+          messages = @user.errors.full_messages.map { |msg| msg }.join
+          flash[:error] = "Error: " + messages
+          redirect_to new_invitation_admin_users_path
+      end
+  end
+
 end
