e1c8ec704f26196a417ad51226ee0ad2d86a2074
Paul Storberg <Paul@storberg.net>
2018-07-21 13:33:05 -0400

    edited the css code for the background image and precompiled assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
 
   # Generate digests for assets URLs
   config.assets.digest = true
-  
+
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
