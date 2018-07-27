daca3e8210e104ea6c6d0daa557555c3dfecf65e
Paul Storberg <Paul@storberg.net>
2018-07-27 09:28:57 -0400

    fixed error massage style

---
+++ app/assets/javascripts/alerts.js
@@ -0,0 +1,11 @@
+function slideUp_messages() {
+  var flash_messages = $('.alert').length
+  var timeout = 2000 + (flash_messages-1) * 1000
+  return setTimeout((function() {
+    $('.alert').each(function() {
+      if ($(this).find('a').length == 0) {
+        $(this).slideUp();
+      };
+    });
+  }), timeout);
+};
